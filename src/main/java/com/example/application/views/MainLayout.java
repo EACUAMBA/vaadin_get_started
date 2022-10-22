0c32d2a18dcc176cbf01f63a340fd5baf8238fcb
Edilson Alexandre Cuamba <edilsoncuamba@gmail.com>
2022-10-22 21:48:29 +0200

    adding the logout button.

--- src/main/java/com/example/application/views/MainLayout.java
+++ src/main/java/com/example/application/views/MainLayout.java
@@ -1,8 +1,10 @@
 package com.example.application.views;
 
+import com.example.application.security.SecurityService;
 import com.example.application.views.list.ListView;
 import com.vaadin.flow.component.applayout.AppLayout;
 import com.vaadin.flow.component.applayout.DrawerToggle;
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
@@ -12,7 +14,10 @@ import com.vaadin.flow.router.RouterLink;
 
 //AppLayout is a Vaadin layout with a header and a responsive drawer.
 public class MainLayout extends AppLayout {
-    public MainLayout() {
+    //Autowire the SecurityService and save it in a field.
+    private final SecurityService securityService;
+    public MainLayout(SecurityService securityService) {
+        this.securityService = securityService;
         createHeader();
         createDrawer();
     }
@@ -20,14 +25,22 @@ public class MainLayout extends AppLayout {
     private void createHeader() {
         H1 logo = new H1("Vaadin CRM");
         logo.addClassNames("text-l", "m-m");
+
+        //Create a logout button that calls the logout() method in the service.
+        Button logoutButton = new Button("Log out", e -> securityService.logout());
+
         HorizontalLayout header = new HorizontalLayout(
                 //DrawerToggle is a menu button that toggles the visibility of the sidebar.
                 new DrawerToggle(),
-                logo
+                logo,
+                //Add the button to the header layout.
+                logoutButton
         );
 
         //Centers the components in the header along the vertical axis.
         header.setDefaultVerticalComponentAlignment(FlexComponent.Alignment.CENTER);
+        //Call header.expand(logo) to make the logo take up all the extra space in the layout. This pushes the logout button to the far right.
+        header.expand(logo);
         header.addClassNames("py-0", "px-m");
 
         //Adds the header layout to the application layout’s nav bar, the section at the top of the screen.
