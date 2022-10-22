6568864b43fac2113ae568667696c6c67e86ebaf
Edilson Alexandre Cuamba <edilsoncuamba@gmail.com>
2022-10-22 12:05:55 +0200

    performing the bind of the bean to the form.

--- src/main/java/com/example/application/views/list/ContactForm.java
+++ src/main/java/com/example/application/views/list/ContactForm.java
@@ -1,6 +1,7 @@
 package com.example.application.views.list;
 
 import com.example.application.data.entity.Company;
+import com.example.application.data.entity.Contact;
 import com.example.application.data.entity.Status;
 import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.button.Button;
@@ -10,21 +11,33 @@ import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.textfield.EmailField;
 import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.binder.BeanValidationBinder;
+import com.vaadin.flow.data.binder.Binder;
+import com.vaadin.flow.data.binder.PropertyId;
 
 import java.util.List;
 
 //ContactForm extends FormLayout: a responsive layout that shows form fields in 1 or 2 columns depending on viewport width.
 public class ContactForm extends FormLayout {
     //Creates all the UI components as fields in the component.
+    @PropertyId("firstName")
     TextField firstNameTextField = new TextField("First name");
+    @PropertyId("lastName")
     TextField lastNameTextField = new TextField("Last name");
+    @PropertyId("email")
     EmailField emailField = new EmailField("Email");
+    @PropertyId("status")
     ComboBox<Status> statusComboBox = new ComboBox<>("Status");
+    @PropertyId("company")
     ComboBox<Company> companyComboBox = new ComboBox<>("Company");
 
     Button saveButton = new Button("Save");
     Button deleteButton = new Button("Delete");
     Button cancelButton = new Button("Cancel");
+
+    //BeanValidationBinder is a Binder that’s aware of bean validation annotations. By passing it in the Contact.class, you define the type of object you are binding to.
+    Binder<Contact> contactBinder = new BeanValidationBinder<>(Contact.class);
+    private Contact contact;
 
     public ContactForm(
             List<Company> companyList,
@@ -32,6 +45,9 @@ public class ContactForm extends FormLayout {
     ) {
         //Gives the component a CSS class name, so you can style it later.
         addClassName("contact-form");
+
+        //bindInstanceFields() matches fields in Contact and ContactForm based on their names.
+        contactBinder.bindInstanceFields(this);
 
         companyComboBox.setItems(companyList);
         companyComboBox.setItemLabelGenerator(Company::getName);
@@ -66,5 +82,12 @@ public class ContactForm extends FormLayout {
                 deleteButton,
                 cancelButton
         );
+    }
+
+    public void setContact(Contact contact){
+        //Save a reference to the contact, so you can save the form values back into it later.
+        this.contact = contact;
+        //Calls binder.readBean() to bind the values from the contact to the UI fields. readBean() copies the values from the contact to an internal model; that way you don’t overwrite values if you cancel editing.
+        contactBinder.readBean(contact);
     }
 }
